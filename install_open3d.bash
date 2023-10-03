236ab969ba43750b3d536760e8e4e1c0d8894c31
ivdmne <v.271998@yandex.ru>
2023-10-03 23:01:21 +0300

    refactor

--- install_open3d.bash
+++ install_open3d.bash
@@ -4,10 +4,6 @@ VERSION=0.15.1
 
 [ ! -d Open3D ] && git clone https://github.com/isl-org/Open3D.git -b v$VERSION || echo "Skip pulling repo"
 
-# cd Open3D
-# cmake -DCMAKE_BUILD_TYPE=Release -Wno-dev -DBUILD_EXAMPLES=OFF -DBUILD_PYTHON_MODULE=OFF -DBUILD_ISPC_MODULE=OFF -DBUILD_WEBRTC=OFF --install ./build -S ./ -B ./build
-
-
 [ ! -d Open3D/build ] && mkdir Open3D/build
 cd Open3D/build
 
@@ -22,4 +18,5 @@ cmake .. \
 
 make -j$(nproc)
 
+echo "Applying 'sudo make install'. Enter password"
 sudo make install
